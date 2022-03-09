89dc9acd71ae992c618b583c21dcbbb518204d00
Adonay Douglas <68573705+Abominattion@users.noreply.github.com>
2022-03-09 13:39:40 -0300

    Add files via upload

---
+++ dist/assets/TesteView.1a4bc14c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.6f6844dd.js";import{o as r,c as t}from"./vendor.90ddb25a.js";const o={};function c(a,n){return r(),t("h1",null,"rota teste")}var f=e(o,[["render",c]]);export{f as default};
